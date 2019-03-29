c6bc363f6b269042471e769ae297c545c79af068
youngduk.park <youngduk.park@nhn.com>
2019-03-29 16:01:46 +0900

    build test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-function component() {
-    var element = document.createElement('div');
-
-    element.innerHTML = 'Hello Webpack';
-
-    return element;
-}
-
-document.body.appendChild(component());
-
 export const hihi = 'hihi';
