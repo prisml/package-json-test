349a9c616368c15457d3e4b1697847b44cc675c8
youngduk.park <youngduk.park@nhn.com>
2019-04-01 12:11:16 +0900

    externals test

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,11 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        library: 'hihi',
+        // library: 'hihi',
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    externals: {
+        hihi: 'hihi',
     },
 };
